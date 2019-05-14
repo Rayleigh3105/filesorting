c91b6d4433ee87affffeec6a57c0935d2f4c538c
mvogt <moritz.vogt@vogges.de>
2019-05-14 22:42:44 +0200

    initial Commit

---
+++ src/main/java/com/modev/filesorting/FilesortingApplication.java
@@ -0,0 +1,13 @@
+package com.modev.filesorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesortingApplication.class, args);
+	}
+
+}
